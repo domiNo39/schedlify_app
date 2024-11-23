c5e7b54d91d8f657c708606b74bfc04225039326
vasab1tch <cherpakvasyl@gmail.com>
2024-11-23 16:05:42 +0200

    dwadwa

---
+++ Schedlify/Controllers/ClassController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schedlify.Data;
+using Schedlify.Repositories;
+using Schedlify.Models;
+
+namespace Schedlify.Controllers
+{
+    public class UniversityController
+    {
+        private readonly UniversityRepository universityRepository;
+        public UniversityController()
+        {
+            ApplicationDbContext _context = ApplicationDbContextFactory.CreateDbContext();
+            universityRepository = new UniversityRepository(_context);
+        }
+        public IEnumerable<University> Search(string namePart)
+        {
+            var universities = universityRepository.GetByNamePart(namePart);
+            return universities;
+        }
+
+        public University? Add(string name)
+        {
+            var existingUniversity = universityRepository.GetByName(name);
+            if (existingUniversity != null)
+            {
+                return null;
+            }
+
+            var newUniversity = universityRepository.Add(name);
+
+            return newUniversity; 
+        }
+
+
+
+    }
+}
---
+++ Schedlify/Controllers/TemplateSlotController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schedlify.Data;
+using Schedlify.Repositories;
+using Schedlify.Models;
+
+namespace Schedlify.Controllers
+{
+    public class TemplateSlotController
+    {
+        private readonly TemplateSlotRepository templateSlotRepository;
+        public TemplateSlotController()
+        {
+            ApplicationDbContext _context = ApplicationDbContextFactory.CreateDbContext();
+            templateSlotRepository = new TemplateSlotRepository(_context);
+        }
+        public IEnumerable<TemplateSlot> GetByDepartmentId(Guid departmentId)
+        {
+            var templateSlots = templateSlotRepository.GetByDepartmentId(departmentId)
+            return templateSlots;
+        }
+
+        public TemplateSlot? GetByDepartmentId(Guid departmentId)
+        {
+            var newUniversity = templateSlotRepository.GetByDepartmentId(departmentId);
+            return newUniversity; 
+        }
+
+
+
+    }
+}
