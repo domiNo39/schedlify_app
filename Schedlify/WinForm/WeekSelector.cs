c3c6cc8aa6b9654df37109f003ab1f4ff38354d5
vasab1tch <cherpakvasyl@gmail.com>
2024-11-24 23:13:56 +0200

    week selector

--- Schedlify/WinForm/WeekSelector.cs
+++ Schedlify/WinForm/WeekSelector.cs
@@ -12,27 +12,41 @@ namespace Schedlify.WinForm
 {
     public partial class WeekSelector : UserControl
     {
-        public DateOnly curr_week_start;
-        public DateOnly curr_week_end;
+        public DateOnly CurrWeekStart;
         public WeekSelector()
         {
+            DateOnly nowDate = DateOnly.FromDateTime(DateTime.Now);
+            this.CurrWeekStart = nowDate.AddDays((((int)nowDate.DayOfWeek + 6) % 7) * -1);
             InitializeComponent();
+            InitializeTime();
+
+
+        }
+        private void InitializeTime()
+        {
+            dateTimePicker1.Value = CurrWeekStart.ToDateTime(new TimeOnly(0,0));
         }
 
 
         private void button_prev_week_Click(object sender, EventArgs e)
         {
+            this.CurrWeekStart = CurrWeekStart.AddDays(7);
+            InitializeTime();
+
 
         }
         private void button_next_week_Click(object sender, EventArgs e)
         {
+            this.CurrWeekStart = CurrWeekStart.AddDays(-7);
+            InitializeTime();
 
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-            DateOnly curr_selected_date = DateOnly.FromDateTime(dateTimePicker1.Value.Date);
-
+            DateOnly curr_selected_date = DateOnly.FromDateTime(dateTimePicker1.Value);
+            this.CurrWeekStart = curr_selected_date.AddDays((((int)curr_selected_date.DayOfWeek + 6) % 7) * -1);
+            InitializeTime();
 
         }
     }
